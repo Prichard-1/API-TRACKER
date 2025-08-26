7fff8a60be8e5ee216bc0614d0668731f6f3523a
Prichard Dube <genarodube8@gmail.com>
2025-08-26 21:36:54 +0200

    register Activity model in Django admin

--- fitness_tracker/activities/admin.py
+++ fitness_tracker/activities/admin.py
@@ -1,3 +1,10 @@
+# activities/admin.py
 from django.contrib import admin
+from .models import Activity
 
-# Register your models here.
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('user', 'activity_type', 'duration', 'distance', 'calories_burned', 'date')
+    list_filter = ('activity_type', 'date')
+    search_fields = ('user__username', 'activity_type')
+    ordering = ('-date',)
