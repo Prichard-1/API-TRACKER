e995df6d90b262eb36ff5d32ef284f6df30a997c
Prichard Dube <genarodube8@gmail.com>
2025-09-07 11:18:44 +0200

    add settings.py

--- fitness_tracker/users/views.py
+++ fitness_tracker/users/views.py
@@ -1,8 +1,17 @@
-from rest_framework import viewsets, permissions
+# users/views.py
+from rest_framework import generics, viewsets, permissions
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
+# Registration endpoint
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]  # anyone can register
+
+# User management endpoint
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]  # only logged-in users
+
