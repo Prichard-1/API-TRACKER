a9d3bb61faf2b0a9e8c24f48823a07f02433d836
Prichard Dube <genarodube8@gmail.com>
2025-09-07 12:19:37 +0200

    added main app url configurations

--- activities/urls.py
+++ activities/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ActivityViewSet, ActivitySummaryView
+from .views import ActivityViewSet, ActivitySummaryView, GoalViewSet, PlanViewSet
 
 router = DefaultRouter()
-router.register(r'', ActivityViewSet, basename='activity')
+router.register(r'activities', ActivityViewSet, basename='activity')
+router.register(r'goals', GoalViewSet, basename='goal')
+router.register(r'plans', PlanViewSet, basename='plan')
 
 urlpatterns = [
-    path('summary/', ActivitySummaryView.as_view(), name='activity-summary'),
+    # Activity summary endpoint
+    path('activities/summary/', ActivitySummaryView.as_view(), name='activity-summary'),
+
+    # Router URLs for activities, goals, and plans
     path('', include(router.urls)),
 ]
+
